0cb7a9b51752e50d32f7bfb7894aa5c4f0d46ae3
Cody-Joe Abe <cabe@cs.uoregon.edu>
2018-10-22 22:22:18 -0700

    fixed some webpack/babel problems

--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,12 @@ module.exports = {
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./js/client.js",
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-        }
+
       }
     ]
   },
