c5c59d178efd46aac23e19a293c3eef45106e463
Cody-Joe Abe <cabe@cs.uoregon.edu>
2018-10-23 23:34:31 -0700

    lots of scss editing

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -2,18 +2,14 @@ import React from "react"
 
 import Header from "./header";
 import Body from "./body";
-import Footer from "./footer";
 import SideBar from "./sidebar"
 export default class Layout extends React.Component {
     render() {
-        return (
-            <div>
-                <Header/>
-                <Body/>
-                <SideBar/>
+        return (<div>
+            <Header/>
+            <Body/>
+            <SideBar/>
 
-                <Footer/>
-            </div>
-        )
+        </div>)
     }
 }
--- src/js/components/sidebar.js
+++ src/js/components/sidebar.js
@@ -6,23 +6,29 @@ export default class Header extends React.Component {
     render() {
         return (
             <div className="side-bar">
-                <SideNav defaultSelectedPath="1">
-                    <div className="side-bar-item">
-                        <Nav id="1">
-                            Item 1
+                <div className="side-nav">
+                    <SideNav defaultSelectedPath="1" >
+                        <Nav id="l" className="side-bar-item-logo">
+                            Logo
                         </Nav>
-                    </div>
-                    <div className="side-bar-item">
-                        <Nav id="2">
-                            Item 2
+                        <Nav id="1" className="side-bar-item">
+                            League of Legends
                         </Nav>
-                    </div>
-                    <div className="side-bar-item">
-                        <Nav id="3">
-                            Item 3
+                        <Nav id="2" className="side-bar-item">
+                            Fortnite
                         </Nav>
-                    </div>
-                </SideNav>
+                        <Nav id="3" className="side-bar-item">
+                            Rocket League
+                        </Nav>
+                    </SideNav>
+                </div>
+                <div className="side-nav-settings">
+                    <SideNav>
+                        <Nav id="s" className="side-bar-item-settings">
+                            Settings
+                        </Nav>
+                    </SideNav>
+                </div>
             </div>
         )
     }
